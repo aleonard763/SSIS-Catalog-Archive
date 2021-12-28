11e44f94d53c747224aa9eeb6664ba643b223b21
aleonard763 <andy.leonard@gmail.com>
2021-12-28 15:09:56 -0500

    Added delta counts to compare script

--- sql/SSISDBArchive_SSISDB_Counts_Compare.sql
+++ sql/SSISDBArchive_SSISDB_Counts_Compare.sql
@@ -20,6 +20,12 @@ Select
 ,
    (Select Count(*) As OperationsCount
     From SSISDBArchive.internal.operations) As SSISDBArchive_OperationsCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.operations)
+   -
+   (Select Count(*) As OperationsCount
+    From SSISDBArchive.internal.operations)) As Delta_OperationsCount
 
 Select
    (Select Count(*)
@@ -27,6 +33,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.executables) As SSISDBArchive_ExecutablesCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.executables)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.executables)) As Delta_ExecutablesCount
 
 Select
    (Select Count(*)
@@ -34,6 +46,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.executions) As SSISDBArchive_ExecutionsCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.executions)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.executions)) As Delta_ExecutionsCount
 
 Select
    (Select Count(*)
@@ -41,6 +59,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.validations) As SSISDBArchive_ValidationsCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.validations)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.validations)) As Delta_ValidationsCount
 
 Select
    (Select Count(*)
@@ -48,6 +72,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.operation_os_sys_info) As SSISDBArchive_Operation_os_sys_infoCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.operation_os_sys_info)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.operation_os_sys_info)) As Delta_Operation_os_sys_infoCount
 
 Select
    (Select Count(*)
@@ -55,6 +85,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.operation_messages) As SSISDBArchive_Operation_messagesCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.operation_messages)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.operation_messages)) As Delta_Operation_messagesCount
 
 Select
    (Select Count(*)
@@ -62,6 +98,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.extended_operation_info) As SSISDBArchive_Extended_operation_infoCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.extended_operation_info)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.extended_operation_info)) As Delta_Extended_operation_infoCount
 
 Select
    (Select Count(*)
@@ -69,6 +111,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.event_messages) As SSISDBArchive_Event_messagesCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.event_messages)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.event_messages)) As Delta_Event_messagesCount
 
 Select
    (Select Count(*)
@@ -76,6 +124,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.execution_component_phases) As SSISDBArchive_Executable_component_phasesCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.execution_component_phases)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.execution_component_phases)) As Delta_Executable_component_phasesCount
 
 Select
    (Select Count(*)
@@ -83,6 +137,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.executable_statistics) As SSISDBArchive_Executable_statisticsCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.executable_statistics)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.executable_statistics)) As Delta_Executable_statisticsCount
 
 Select
    (Select Count(*)
@@ -90,6 +150,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.execution_data_taps) As SSISDBArchive_Execution_data_tapsCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.execution_data_taps)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.execution_data_taps)) As Delta_Execution_data_tapsCount
 
 Select
    (Select Count(*)
@@ -97,6 +163,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.execution_parameter_values) As SSISDBArchive_Execution_parameter_valuesCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.execution_parameter_values)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.execution_parameter_values)) As Delta_Execution_parameter_valuesCount
 
 Select
    (Select Count(*)
@@ -104,6 +176,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.execution_property_override_values) As SSISDBArchive_Execution_property_override_valuesCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.execution_property_override_values)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.execution_property_override_values)) As Delta_Execution_property_override_valuesCount
 
 Select
    (Select Count(*)
@@ -111,6 +189,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.execution_data_statistics) As SSISDBArchive_Execution_data_statisticsCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.execution_data_statistics)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.execution_data_statistics)) As Delta_Execution_data_statisticsCount
 
 Select
    (Select Count(*)
@@ -118,6 +202,12 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.event_message_context) As SSISDBArchive_Event_message_contextCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.event_message_context)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.event_message_context)) As Delta_Event_message_contextCount
 
 Select
    (Select Count(*)
@@ -125,6 +215,13 @@ Select
 ,
    (Select Count(*)
     From SSISDBArchive.internal.operation_permissions) As SSISDBArchive_Operation_permissionsCount
+,
+   Abs((Select Count(*)
+    From SSISDB.internal.operation_permissions)
+	-
+   (Select Count(*)
+    From SSISDBArchive.internal.operation_permissions)) As Delta_Operation_permissionsCount
+
  end
 Else
  begin
